33228f9d8bc826445ece545a13c7b622ee91c86f
kurra-yuki <k.yuki0731.psan@gmail.com>
2024-04-14 18:42:02 +0900

    Timer refuctering

--- src/componets/timer/UseCountdown.jsx
+++ src/componets/timer/UseCountdown.jsx
@@ -1,17 +1,21 @@
 import { useState, useEffect } from 'react';
 
 export const UseCountdown = () => {
+  const [creationTime, setCreationTime] = useState(Date.now());
   const [timeLeft, setTimeLeft] = useState(24 * 60 * 60); // 初期値は24時間
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setTimeLeft(timeLeft => timeLeft - 1);
+      const currentTime = Date.now();
+      const timePassed = Math.floor((currentTime - creationTime) / 1000);
+      setTimeLeft(timeLeft => timeLeft - timePassed);
+      setCreationTime(currentTime);
     }, 1000);
 
     return () => {
       clearInterval(timer);
     };
-  }, []);
+  }, [creationTime]);
 
   const hoursLeft = Math.floor(timeLeft / 3600);
   const minutesLeft = Math.floor((timeLeft % 3600) / 60);
